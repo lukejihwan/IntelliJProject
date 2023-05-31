b515f922a83561847ae0fefd3eb495e9ee2f1704
luke <luke9701@naver.com>
2023-05-31 23:45:45 +0900

    설정중

--- src/Controller.java
+++ src/Controller.java
@@ -3,5 +3,6 @@ public class Controller {
     public static void main(String[] args){
         System.out.println("컨트롤러");
         System.out.println("컨트롤러");
+        System.out.println("컨트롤러");
     }
 }
